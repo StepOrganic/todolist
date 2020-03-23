ff906301111068b22df2f6e0f9557d493184cdb6
SWbottier <samuelwaybespoke@gmail.com>
2020-03-23 22:25:58 +0800

    3 fixes

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-// const LOCAL_STORAGE_KEY = "toDoList";
+const LOCAL_STORAGE_KEY = "toDoList";
+const inputBox = document.getElementById("input-box");
 
-// // Starts here, once JS is loaded
-// // checks if localStorage contains a todo list
-// // if there is NO todo list, initialize local storage with empty list
-// if (!localStorage.getItem(LOCAL_STORAGE_KEY)) {
-//   initStorageWithEmptyToDoList();
-// } else {
-//   // else run an update, so it renders the UI with the existing list.
-//   updateToDoList();
-// }
+// Starts here, once JS is loaded
+// checks if localStorage contains a todo list
+// if there is NO todo list, initialize local storage with empty list
+if (!localStorage.getItem(LOCAL_STORAGE_KEY)) {
+  initStorageWithEmptyToDoList();
+} else {
+  // else run an update, so it renders the UI with the existing list.
+  updateToDoList();
+}
 
-// function initStorageWithEmptyToDoList() {
-//   localStorage.setItem(LOCAL_STORAGE_KEY, []);
-//   updateToDoList();
-// }
+function initStorageWithEmptyToDoList() {
+  localStorage.setItem(LOCAL_STORAGE_KEY, []);
+  updateToDoList();
+}
 
 function updateToDoList() {
   // get current list
-//   const currentList = localStorage.getItem(LOCAL_STORAGE_KEY);
+  const currentList = localStorage.getItem(LOCAL_STORAGE_KEY);
   
   // get new item from input
   const newToDoItem = getNewToDoItem();
   console.log(newToDoItem);
   
   // append new item to list
-  addItemToListUI(newToDoItem);
+  // addItemToListUI(newToDoItem);
+  addItemToList(newToDoItem);
+
 }
 
 function getNewToDoItem() {
@@ -32,27 +35,36 @@ function getNewToDoItem() {
   return inputBox.value;
 }
 
-// function addItemToList(item) {
-//   // update storage
-//   addItemToLocalStorage(newToDoItem);
+function addItemToList(item) {
+  // update storage
+  addItemToLocalStorage(item);
 
-//   //ToDo: instead of adding per item,
-//   // refresh UI with the entire new ToDo list.
-//   addItemToListUI(newToDoItem);
-// }
+  //ToDo: instead of adding per item,
+  // refresh UI with the entire new ToDo list.
+  addItemToListUI(item);
+}
 
-// function addItemToLocalStorage(item) {
-//   localStorage.setItem(LOCAL_STORAGE_KEY, [item]);
-// }
+function addItemToLocalStorage(item) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, [item]);
+}
 
 function addItemToListUI(item) {
   const container = document.getElementById("display-list");
-
-  container.insertAdjacentHTML(
-    "beforeend",
-    '<li class="btn btn-light">' + item + "</li>"
-  );
+  if(item.length >0){
+    container.insertAdjacentHTML(
+      "beforeend",
+      "<li class='btn btn-light' id='task-item'>" + item + "</li>"
+    );
+  }
 }
+
 const formToSubmit = document.getElementById("toDoForm");
-    function handleForm(event) { event.preventDefault(); } 
+    function handleForm(event) { 
+      event.preventDefault(); 
+      inputBox.value = "";
+    } 
     formToSubmit.addEventListener('submit', handleForm);
+
+    
+    
+    
